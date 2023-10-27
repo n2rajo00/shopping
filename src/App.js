ef6baaf2f9c882bcbbd5ba910cf4ae52d89fa0e2
n2rajo00 <128301054+n2rajo00@users.noreply.github.com>
2023-10-27 17:16:23 +0300

    valmis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
+
+const URL = 'http://localhost:3001/'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [items, setItems] = useState([])
+  const [item, setItem] = useState()
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItems(response.data)
+      }).catch(error => {
+        alert(error.response.data.error)
+      })
+  }, [])
+
+
+function save() {
+  const json = JSON.stringify({ description: item })
+  axios.post(URL + 'new', json, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((response) => {
+      const addedObject = JSON.parse(json)
+      addedObject.id = response.data.id
+      setItems(items => [...items, addedObject])
+      setItem('')
+    }).catch(error => {
+      alert(error.response.data.error)
+    })
+}
+
+function remove(id) {
+  axios.delete(`${URL}delete/${id}`)
+    .then(() => {
+      const newListWithoutRemoved = items.filter((item) => item.id !== id)
+      setItems(newListWithoutRemoved)
+    }).catch(error => {
+      alert(error.response.data.error)
+    })
+}
+
+return (
+  <div>
+    <h3>My shoppinglist</h3>
+    <form>
+      <label>Add new</label>
+      <input value={item} onChange={e => setItem(e.target.value)} />
+      <button type='button' onClick={save}>Save</button>
+    </form>
+    <ol>
+      {items.map(items => (
+        <li key={items.id}>{items.description} </li>
+      ))}
+    </ol>
+    <ol>
+      {items.map(item => (
+        <li key={item.id}>{item.description} <a href='#' onClick={() => remove(item.id)}>Delete</a></li>
+      ))}
+    </ol>
+  </div>
+);
+ 
 }
 
 export default App;
